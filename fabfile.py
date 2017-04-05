6a04837f9b8bb91ce84d6fdc6875d42495441b35
Jon <alangwansui@gmail.com>
2017-04-05 17:51:03 +0800

    Create fabfile.py

---
+++ fabfile.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import fabric
+from fabric.api import local, cd, run
+
+
+def git_push():
+  #1: git pull
+  
+  #2:git commit 
+  
+  #:git push
+  
+  
+ def remote_git_pull():
+ 
+ 
+ def remove_odoo_restart():
+   #pass
+   
+   
+ 
+ 
+ 
+  
